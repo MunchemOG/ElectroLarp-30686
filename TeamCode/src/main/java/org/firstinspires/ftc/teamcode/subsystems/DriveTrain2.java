120a27c8055cc4215a776c68144b97f58b5c3e48
AxeLotlol <mokshnot5@gmail.com>
2026-08-29 04:08:13 -0500

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveTrain2.java
@@ -212,9 +212,8 @@ public class DriveTrain2 implements Subsystem {
 
     private ServoImplEx turret1;
     private ServoImplEx turret2;
-
-    public static double turretOffset = 0;
-    public static double turretOffset2 = 0;
+    public static double turretOffset = 15;
+    public static double turretOffset2 = -15;
     public static double turretOffsetStep = -5;
     // Inches from the Pinpoint/Pedro robot pose origin to the turret pivot.
     public static double turretForwardOffset = -0.52588;
